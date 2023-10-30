8e6cf4efa329f4e701ac95be850fd49ff0cfb493
Alvaro0004 <alvarollano04@gmail.com>
2023-10-30 10:37:06 -0300

    Working with list, set and tuple

---
+++ 06_advanced_set_operations/myCode.py
@@ -0,0 +1,9 @@
+# Exercises
+
+# This coding exercise requires you to complete three steps:
+# 1. Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+# 2. Create a tuple, called my_tuple, with a single value in it
+# 3. Modify the variable set2 so that set1.intersection (set2) returns {5, 77, 9, 12}
+
+my_list = [25, 25, 50]
+my_tuple = (100,)
