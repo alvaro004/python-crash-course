f64d57fcd36a7ab63be7deced7a8ad88221a81f5
Alvaro0004 <alvarollano04@gmail.com>
2023-10-31 14:26:31 -0300

    Using map function

--- 19_lambda_functions/myCode.py
+++ 19_lambda_functions/myCode.py
@@ -1,6 +1,6 @@
 # Calculate the square of a secuence of numbers
 
-squareNum = [x**2 for x in range(1, 10)]
+squareNum = [x**2 for x in range(1, 11)]
 print(f"First Square {squareNum}")
 
 
@@ -11,6 +11,15 @@ def square(x):
     return x**2
 
 
-squareNum2 = [square(x) for x in range(1, 10)]
+squareNum2 = [square(x) for x in range(1, 11)]
 
 print(f"Second Square {squareNum2}")
+
+
+# Now using map
+
+sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+squareNum3 = map(square, range(1, 11))
+
+print(f"Third Square {list(squareNum3)}")
