4055f73fbf2c752123593ec483fc358f0b5e6141
Alvaro0004 <alvarollano04@gmail.com>
2023-10-31 13:31:37 -0300

    Still working with list comprenhention

--- 12_list_comprehension/myCode.py
+++ 12_list_comprehension/myCode.py
@@ -5,3 +5,21 @@ people = [2000, 1998, 1993, 2004, 2006]
 ages = [p for p in people if p >= 2000]
 
 print(ages)
+
+# Create a list of squares of numbers from 1 to 10 using a list comprehension.
+
+numbers = [x**2 for x in range(1, 11)]
+
+print(numbers)
+
+# Create a list of the lengths of words in a sentence using a list comprehension.
+
+sentence = "Hello, this is a sentence to test"
+
+sentenceSplit = sentence.split()
+
+sentenceLen = [len(s) for s in sentenceSplit]
+
+print(sentenceSplit)
+print("############################33")
+print(sentenceLen)
